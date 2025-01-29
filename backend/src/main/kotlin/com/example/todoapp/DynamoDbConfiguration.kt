6fee726f9c9ec0d468bcf43e8e6f291107b4dc23
Yusuke Shinyama <yusuke@bekindslab.com>
2025-01-29 17:08:49 +0900

    Added: dummy credentials.

--- backend/src/main/kotlin/com/example/todoapp/DynamoDbConfiguration.kt
+++ backend/src/main/kotlin/com/example/todoapp/DynamoDbConfiguration.kt
@@ -3,6 +3,10 @@ package com.example.todoapp
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import java.net.URI
@@ -16,9 +20,15 @@ class DynamoDbConfiguration(
 ) {
     @Bean
     fun dynamoDbClient(): DynamoDbClient {
+        val dummyCredentials = AwsBasicCredentials.create("xxx", "yyy")
         return DynamoDbClient.builder()
             .endpointOverride(URI.create(endpoint))
             .region(Region.of(region))
+            .credentialsProvider(
+                AwsCredentialsProviderChain.of(
+                StaticCredentialsProvider.create(dummyCredentials),
+                DefaultCredentialsProvider.create(),
+            ))
             .build()
     }
 }
