6c62f1b753353fb080ae2f72ad5285057b86eca5
Yusuke Shinyama <yusuke@bekindslab.com>
2025-02-04 13:22:40 +0900

    Added: docker compose support for runtime.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("software.amazon.awssdk:dynamodb:2.30.2")
 	runtimeOnly("org.springframework.boot:spring-boot-starter-actuator")
+	runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
