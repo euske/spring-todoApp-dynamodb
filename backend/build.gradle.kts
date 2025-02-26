27ad3214b411f6f5a61df65d8ad8e291f82768c1
Yusuke Shinyama <yusuke@bekindlabs.com>
2025-02-26 13:52:22 +0900

    Added: JaCoCo, Java Code Coverage.
    Signed-off-by: Yusuke Shinyama <yusuke@bekindlabs.com>

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
+	jacoco
 }
 
 group = "com.example"
@@ -53,3 +54,12 @@ tasks.named<JavaExec>("bootRun") {
 		}
 	}
 }
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
