9de53c319cfa4ead91e4b9674bbead18f312359e
Yusuke Shinyama <yusuke@bekindlabs.com>
2025-03-26 11:01:25 +0900

    Added: e2e Makefile.
    Signed-off-by: Yusuke Shinyama <yusuke@bekindlabs.com>

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -8,6 +8,8 @@ import { defineConfig, devices } from '@playwright/test';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+const PORT = process.env.PORT || '5173';
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -22,11 +24,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173',
+    baseURL: `http://localhost:${PORT}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
