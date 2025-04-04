af61442e4b7ef9b2d13a5dd1cc8176ea5e9999dd
Yusuke Shinyama <yusuke@bekindlabs.com>
2025-04-04 19:05:22 +0900

    Changed: docker-compose
    Signed-off-by: Yusuke Shinyama <yusuke@bekindlabs.com>

--- backend-spring/localstack/init.sh
+++ backend-spring/localstack/init.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -x
 AWSCLI=${AWSCLI:-awslocal}
+REGION=${REGION:-ap-northeast-1}
+
+TABLE_NAME=todo
 
 $AWSCLI dynamodb create-table \
-  --region ap-northeast-1 \
+  --region $REGION \
   --billing-mode PAY_PER_REQUEST \
-  --table-name todo \
+  --table-name $TABLE_NAME \
   --attribute-definitions \
     AttributeName=id,AttributeType=S \
   --key-schema \
