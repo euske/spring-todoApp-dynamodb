be4a2b12f67788e4599aecf3c3af17c70ba5f116
Yusuke Shinyama <yusuke@bekindlabs.com>
2025-07-08 09:52:26 +0900

    Added: e2e delete item.

--- e2e/tests/app.spec.ts
+++ e2e/tests/app.spec.ts
@@ -1,19 +1,27 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 import {randomUUID} from "node:crypto";
 
-test('has title', async ({ page }) => {
+test('アプリのタイトルが見える。', async ({page}) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/TODO/);
 });
 
-test('can post an item', async ({ page }) => {
+test('項目を追加・削除できる。', async ({page}) => {
   const text = `todo${randomUUID()}`;
-  
+
   await page.goto('/');
 
-  await page.getByRole('textbox').fill(text);
-  await page.getByRole('button', {name: 'Add'}).click();
-  await expect(page.getByRole('listitem').getByText(text)).toBeVisible()
+  await test.step('項目を追加する。', async () => {
+    await page.getByRole('textbox').fill(text);
+    await page.getByRole('button', {name: 'Add'}).click();
+    await expect(page.getByRole('listitem').getByText(text)).toBeVisible()
+  })
+
+  await test.step('追加した項目を削除する。', async () => {
+    const item = page.getByRole('listitem').getByText(text)
+    await item.getByRole('button', {name: 'Delete'}).click();
+    await expect(page.getByRole('listitem').getByText(text)).not.toBeVisible()
+  })
 });
